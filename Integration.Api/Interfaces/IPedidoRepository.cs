ac2cf0f055afb9e0ba5a3866c80228492bc573b9
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-02 15:39:50 -0300

    implementa conversor de data only para int

---
+++ Integration.Api/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Integration.Api.Models;
+
+namespace Integration.Api.Interfaces
+{
+    public interface IPedidoRepository
+    {
+        Task<IEnumerable<Pedido>> SelecionarTodos();
+        Task<Pedido> SelecionarPorInfo(int local, string serie, DateOnly data, int numero)
+        {
+            
+        }
+    }
+}
--- Integration.Api/obj/Debug/net8.0/Integration.Api.AssemblyInfo.cs
+++ Integration.Api/obj/Debug/net8.0/Integration.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Integration.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465f17db195566ca77378c7fb84b3833d04ec1e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+808e318d5095524da5c69726304269bda263c69c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Integration.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Integration.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
