d5982d7f83d65f7695f01d3795d48e41ce98348e
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-06 02:00:52 -0300

    feat: add paginação em Cliente

--- Integration.Application/Interfaces/IClienteService.cs
+++ Integration.Application/Interfaces/IClienteService.cs
@@ -6,7 +6,7 @@ namespace Integration.Application.Interfaces
 {
     public interface IClienteService
     {
-        Task<List<ClienteDto>> BuscarTodos();
+        Task<PaginacaoDto<ClienteDto>> BuscarTodos(int pagina, int tamanhoPagina);
         Task<ClienteDetalhesDto> BuscarPorId(int id);
     }
 }
